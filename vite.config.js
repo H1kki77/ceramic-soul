666609ae5d2acbac5a54521c500d6837d6254dfc
H1kki77 <vkorobay@gmail.com>
2026-02-08 18:19:29 +0200

    fix paths for gh pages

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,14 @@ import viteImagemin from 'vite-plugin-imagemin'
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+	base: '/ceramic-soul/',
 	build: {
 		rollupOptions: {
 			input: {
 				main: resolve(__dirname, "index.html"),
-				catalog: "catalog.html",
-				blog: "blog.html",
-				about: "about.html",
+				catalog: resolve(__dirname, "catalog.html"),
+				blog: resolve(__dirname, "blog.html"),
+				about: resolve(__dirname, "about.html"),
 			},
 		},
 	},
